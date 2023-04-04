715382d3614ae5841cf353f4089fc339ed13fb52
OsbaldoA <luisarmendariz1205@gmail.com>
2023-04-04 12:14:38 -0600

    Update Buscador.jsx

--- src/componentes/Buscador.jsx
+++ src/componentes/Buscador.jsx
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 
 class Buscador extends Component {
 
-  ////Se le agrego esto a la modificación
+  busquedaRef = React.createRef();
+  
+  obtenerDatos = (e) => {
+    e.preventDefault();
+    console.log(this.busquedaRef.current.value);
+  }
+
   state = {};
   render() {
     return (
-      <form>
+      <form onSubmit={this.obtenerDatos}>
         <div className="px-10 pb-10 grid grid-row grid-cols-4 gap-4">
           <div className="col-span-3">
             <input
+            ref={this.busquedaRef}
               type="text"
               className="w-full h-10 px-4 outline-none"
               placeholder="Escribe aquí"
