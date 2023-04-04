e1a08bcc271b981179fb2f356f0abfd7388a3e49
OsbaldoA <luisarmendariz1205@gmail.com>
2023-04-04 16:59:43 -0600

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect, Component } from "react";
 import "./App.css";
 import Buscador from "./componentes/Buscador";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
+
+  const [termino, setTermino] = useState("zdfhg");
+
+  useEffect(()=>{
+    setTermino("Hola");
+  },[]);
 
   return (
     <div>
       <div className="max-w-xl mx-auto bg-slate-400">
-        <h1 className="p-8 h-20 text-center text-white">Buscador de imagenes</h1>
-          <Buscador/>
+        <h1 className="p-8 h-20 text-center text-white">
+          Buscador de imagenes
+        </h1>
+        <Buscador
+        mensaje={termino}/>
       </div>
     </div>
   );
--- src/componentes/Buscador.jsx
+++ src/componentes/Buscador.jsx
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
 
 class Buscador extends Component {
-
   busquedaRef = React.createRef();
-  
+
   obtenerDatos = (e) => {
     e.preventDefault();
-    console.log(this.busquedaRef.current.value);
-  }
+    //console.log(this.busquedaRef.current.value);
+    //this.props.datosBusqueda(this.busquedaRef.current.value);
+    console.log(this.props.mensaje);
+  };
 
   state = {};
   render() {
     return (
       <form onSubmit={this.obtenerDatos}>
         <div className="px-10 pb-10 grid grid-row grid-cols-4 gap-4">
+          
           <div className="col-span-3">
             <input
-            ref={this.busquedaRef}
+              ref={this.busquedaRef}
               type="text"
               className="w-full h-10 px-4 outline-none"
               placeholder="Escribe aquí"
             />
           </div>
           <div>
-            <button className="w-full h-10 bg-red-500 hover:bg-red-400 text-white" type="submit">
+            <button
+              className="w-full h-10 bg-red-500 hover:bg-red-400 text-white"
+              type="submit"
+            >
               Buscar
             </button>
           </div>
